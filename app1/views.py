f7f5ab13d40b7abcd121a7b9efd5d3eddf3645c4
Nvnrocky <narasimhanagulapati01@gmail.com>
2023-11-21 22:26:26 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def prashanth(request):
+    return render(request,'prashanth.html')
---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def bhavani(request):
+    return render(request,'bhavani.html')
